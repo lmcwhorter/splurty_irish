68c2665e696c22fab0f23fa2e88124f1c3f602e1
Laura McWhorter <lmcwhorter@gmail.com>
2017-02-10 01:23:49 +0000

    Added ability to validate user data

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,4 @@
 class Quote < ActiveRecord::Base
-  validates :saying, presence: true
+  validates :saying, presence: true, length: { maximum: 200, minimum: 3}
+    validates :translation, presence: true, length: { maximum: 200, minimum: 3}
 end
