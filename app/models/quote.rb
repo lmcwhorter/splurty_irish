93316722b51a7b06c08f3933d8c744cc989a3d8e
Laura McWhorter <lmcwhorter@gmail.com>
2017-02-11 23:49:31 +0000

    Attempted to resolve new problem of submission form not working

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,4 @@
 class Quote < ActiveRecord::Base
   validates :saying, presence: true, length: { maximum: 200, minimum: 3}
-    validates :translation, presence: true, length: { maximum: 200, minimum: 3}
+  validates :translation, presence: true, length: { maximum: 200, minimum: 3}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
-Splurty::Application.routes.draw do
+Rails.application.routes.draw do
   root 'quotes#index'
+
   resources :quotes
   get 'about', to: 'quotes#about'
   # The priority is based upon order of creation: first created -> highest priority.
